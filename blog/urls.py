53dbad52b61faa5b833426a281acf57a01d6fbfc
xman3636 <xavier.flores3636@gmail.com>
2026-06-14 19:45:22 -0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list') # assigning a post list view to the root url
-    
+    path('', views.post_list, name='post_list'), # assigning a post list view to the root url
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),   
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post # use . since both fviews and models are in the same directory
 
@@ -6,3 +6,7 @@ from .models import Post # use . since both fviews and models are in the same di
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts}) # request is everything we recieve from the user, 'blog...' is the template file, and {} is what were giving the template file
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # automatically handles the case in where a non existant post is requested
+    return render(request, 'blog/post_detail.html', {'post': post})
